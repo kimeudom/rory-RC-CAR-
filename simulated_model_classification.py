513fe6db86f261c1663bca84e809d60b61c7c07d
dom <kimeudom.git@gmail.com>
2023-12-28 10:16:34 +0300

    video feed classifier

--- simulated_model_classification.py
+++ simulated_model_classification.py
@@ -1,26 +1,71 @@
-import bluetooth
-import time
+import cv2
+import mediapipe as mp
 
-# MAC address of the HC-05 Bluetooth module
-hc05_address = 'XX:XX:XX:XX:XX:XX'  # Replace with the MAC address of your HC-05 module
+DIRECTION = "stop"
 
-def send_instruction(instruction):
-    sock.send(instruction)
-    print(f"Sent instruction: {instruction}")
+# Threshold vales for the sensitivity of classification
+# These are manually tuned
+FROWARD_THRESHOLD = 0.09
+LEFT_THRESHOLD = 0
+RIGHT_THRESHOLD = 0.20
 
-try:
-    print("Connecting to HC-05...")
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    sock.connect((hc05_address, 1))
-    print("Connected to HC-05")
+# Video capture object
+cap = cv2.VideoCapture(0)
 
-    instructions = ["forward", "left", "right", "stop"]
+# Hand detection models and hand landmark objects
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
-    for instruction in instructions:
-        send_instruction(instruction)
-        time.sleep(2)  # Adjust the delay as needed
 
-    print("Instructions sent. Closing connection.")
-    sock.close()
-except bluetooth.BluetoothError as e:
-    print(f"Error: {e}")
+# Classify gestures using landmarks
+def classify_gesture(landmarks):
+    # Thumb tip and MCP
+    thumb_tip = landmarks[4]
+    thumb_mcp = landmarks[2]
+
+    # Index fingertip and MCP
+    index_tip = landmarks[8]
+    index_mcp = landmarks[5]
+
+    # Determine gesture
+    # Check orientation for open palm
+    # print(thumb_tip.x - index_tip.x)
+
+    if (thumb_tip.x - index_tip.x) < LEFT_THRESHOLD:
+        return "left"
+    elif (thumb_tip.x - index_tip.x) > RIGHT_THRESHOLD:
+        return "right"
+    elif (thumb_tip.x - index_tip.x) > FROWARD_THRESHOLD:
+        return "forward"
+    else:
+        return "stop"
+
+
+def get_direction():
+    global DIRECTION
+    while True:
+        success, img = cap.read()
+
+        # Converting image colours to RGB
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+        # print(results.multi_hand_landmarks)
+
+        # Check for multiple hands
+        # Extracting each hand
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                gesture = classify_gesture(handLms.landmark)
+                if gesture != DIRECTION:
+                    print(gesture)
+                    DIRECTION = gesture
+
+        cv2.imshow("Live Video Feed", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    get_direction()
