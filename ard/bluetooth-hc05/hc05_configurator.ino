9259258f8135cf07a38118b9928e5577272134df
Taylor Omondi Odhiambo <tayloromondi05@gmail.com>
2024-01-03 16:04:35 +0300

    Arduino can now receive commands wirelessly using bluetooth

--- ard/bluetooth-hc05/hc05_configurator.ino
+++ ard/bluetooth-hc05/hc05_configurator.ino
@@ -13,8 +13,13 @@
  */
 
 #include <SoftwareSerial.h>
-
+//HCO5 address: 98D3:71:F6EFD1
+// +CMODE:1 (One to One)
+// +ROLE:0 (Slave)
+// +NAME:HC05
+// +ADDR:98D3:71:F6EFD1
 // Set the UART Rx and Tx pins
+
 SoftwareSerial BTSerial(10, 11); // Tx, Rx
 
 void setup()
